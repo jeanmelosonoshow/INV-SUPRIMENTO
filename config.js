eb59c841915e0af5f082ba4d8b42c7d60854c4e0
jeanmelosonoshow <jean.melo@sonoshow.com.br>
2026-09-23 20:53:00 -0300

    Update INVENTORY_CONFIG apiUrl to new endpoint

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.INVENTORY_CONFIG = { apiUrl: "" };
+window.INVENTORY_CONFIG = { apiUrl: "https://script.google.com/macros/s/AKfycbyZl0Gel-CYxISZOvVJ9aCMaIHDCF_Ir5rlCqEBUf9TEBYCSHyGtGLXIbPofFYLhZDJ/exec" };
